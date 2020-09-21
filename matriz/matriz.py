6f3a62d4bb87541ded27302e57022a5e9c906f7f
Nicolas Nascimento <ncnascimento1209@gmail.com>
2020-09-21 19:52:03 -0300

    Programa básico para criação de matrizes em python

---
+++ matriz/matriz.py
@@ -0,0 +1,16 @@
+from random import randint
+
+lin = int(input("Digite o número de linhas da matriz:\n"))
+col = int(input("Digite o número de colunas da matriz:\n"))
+
+lista_linha = []
+matriz = []
+
+for i in range (lin):
+    for j in range (col):
+        lista_linha.append(randint(0, 10))
+    matriz.append(lista_linha.copy())
+    lista_linha.clear()
+
+for i in matriz:
+    print(i)
